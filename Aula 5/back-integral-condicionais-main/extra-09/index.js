720ccd6bdb050bc8e74ae5fb6770241180f5c140
Mauricio Taffarel <mauruciotaffarel@gmail.com>
2021-06-14 19:04:53 -0300

    Transformar nota em conceito

--- Aula 5/back-integral-condicionais-main/extra-09/index.js
+++ Aula 5/back-integral-condicionais-main/extra-09/index.js
@@ -1,1 +1,7 @@
 const nota = 8.5;
+
+if (nota>=9) console.log("A");
+else if (nota>=8) console.log("B");
+else if (nota>=6) console.log("C");
+else if (nota>=4) console.log("D");
+else console.log("E");
