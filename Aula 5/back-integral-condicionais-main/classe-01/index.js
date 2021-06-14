f87a3e51fa676ebde3a1c1c2df2188107c6c013c
Mauricio Taffarel <mauruciotaffarel@gmail.com>
2021-06-14 18:17:01 -0300

    correção jokempo

--- Aula 5/back-integral-condicionais-main/classe-01/index.js
+++ Aula 5/back-integral-condicionais-main/classe-01/index.js
@@ -4,13 +4,13 @@ const jogada2 = "tesoura"
 if (jogada1 == jogada2) console.log("empate")
 else {
     if (jogada1 == "pedra") {
-        if (jogada2 == "tesoura") console.log("jogada1");
-        else console.log("jogada2");
+        if (jogada2 == "tesoura") console.log(jogada1);
+        else console.log(jogada2);
     } else if(jogada1 == "tesoura") {
-        if (jogada2 == "papel") console.log("jogada1");
-        else console.log("jogada2");
+        if (jogada2 == "papel") console.log(jogada1);
+        else console.log(jogada2);
     } else {
-        if (jogada2 == "pedra") console.log("jogada1");
-        else console.log("jogada2");
+        if (jogada2 == "pedra") console.log(jogada1);
+        else console.log(jogada2);
     }
 }
