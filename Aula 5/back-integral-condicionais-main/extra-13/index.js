cda9a7f5efad226486ed41b4236318815306ae25
Mauricio Taffarel <mauruciotaffarel@gmail.com>
2021-06-14 20:24:10 -0300

    Compra com Desconto

--- Aula 5/back-integral-condicionais-main/extra-13/index.js
+++ Aula 5/back-integral-condicionais-main/extra-13/index.js
@@ -4,3 +4,11 @@ const tipoDePagamento = "credito";
 //valor da mercadoria (centavos)
 const valorDoProduto = 13000;
 
+var desconto = {
+    "credito": 0.05,
+    "cheque": 0.03,
+    "debito": 0.10,
+    "dinheiro": 0.10
+};
+
+console.log(`Valor a ser pago: R$ ${((1-desconto[tipoDePagamento])*valorDoProduto*0.01).toFixed(2)}`)
