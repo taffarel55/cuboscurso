5e5c4d5349f6227966f47def7867f3b2c0b71976
Mauricio Taffarel <mauruciotaffarel@gmail.com>
2021-06-14 20:35:40 -0300

    Nome para exibição

--- Aula 5/back-integral-condicionais-main/extra-15/index.js
+++ Aula 5/back-integral-condicionais-main/extra-15/index.js
@@ -2,3 +2,6 @@ const primeiroNome = "Mario";
 const sobrenome = "";
 const apelido = "";
 
+if (apelido.length) console.log(apelido);
+else if (sobrenome.length) console.log(primeiroNome+" "+sobrenome); 
+else console.log(primeiroNome);
