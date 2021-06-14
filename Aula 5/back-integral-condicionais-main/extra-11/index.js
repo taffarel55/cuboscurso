a025b8d8546e96ea8d7d6ec6e294a9ca1dd1f3a2
Mauricio Taffarel <mauruciotaffarel@gmail.com>
2021-06-14 20:00:33 -0300

    Valor da parcela do Sucesso Compartilhado 1

--- Aula 5/back-integral-condicionais-main/extra-11/index.js
+++ Aula 5/back-integral-condicionais-main/extra-11/index.js
@@ -6,3 +6,9 @@ const mesesDecorridos = 12;
 
 // Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
 const totalJaPagoPeloAluno = 1000000;
+
+if (mesesDecorridos>=60 || totalJaPagoPeloAluno>=18_000_00) {
+    console.log("Não deve nada");
+} else if (rendaMensalEmCentavos>=2_000_00) {
+    console.log(`O valor da parcela desse mês é R$ ${(0.0018*rendaMensalEmCentavos).toFixed(2)} reais`)
+}
