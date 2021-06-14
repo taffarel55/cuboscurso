31b0d995b3b1db3683c020f4be88ff131babdebf
Mauricio Taffarel <mauruciotaffarel@gmail.com>
2021-06-14 20:49:23 -0300

    Extrato de Compra Online

--- Aula 5/back-integral-condicionais-main/extra-17/index.js
+++ Aula 5/back-integral-condicionais-main/extra-17/index.js
@@ -6,3 +6,6 @@ const quantidadeDoParcelamento = 10;
 
 //valor pago
 const valorPago = 300;
+
+const valorParcela = 0.01*valorDoProduto/quantidadeDoParcelamento;
+console.log(`Restam ${quantidadeDoParcelamento-valorPago/valorParcela} parcelas de R$${valorParcela}`);
