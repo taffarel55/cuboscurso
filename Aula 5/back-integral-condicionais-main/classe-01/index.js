9fd3eac0e67398f3af28606a17b243c8526bf52c
Mauricio Taffarel <mauruciotaffarel@gmail.com>
2021-06-14 18:14:53 -0300

    jokenpo

--- Aula 5/back-integral-condicionais-main/classe-01/index.js
+++ Aula 5/back-integral-condicionais-main/classe-01/index.js
@@ -1,4 +1,16 @@
 const jogada1 = "pedra"
 const jogada2 = "tesoura"
 
-//seu código aqui
+if (jogada1 == jogada2) console.log("empate")
+else {
+    if (jogada1 == "pedra") {
+        if (jogada2 == "tesoura") console.log("jogada1");
+        else console.log("jogada2");
+    } else if(jogada1 == "tesoura") {
+        if (jogada2 == "papel") console.log("jogada1");
+        else console.log("jogada2");
+    } else {
+        if (jogada2 == "pedra") console.log("jogada1");
+        else console.log("jogada2");
+    }
+}
