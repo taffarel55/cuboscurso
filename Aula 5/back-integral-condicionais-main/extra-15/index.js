b94fe617afb8d0cf4a8894ef00f556d1bbe17ec6
Mauricio Taffarel <mauruciotaffarel@gmail.com>
2021-06-14 20:53:08 -0300

    Correção

--- Aula 5/back-integral-condicionais-main/extra-15/index.js
+++ Aula 5/back-integral-condicionais-main/extra-15/index.js
@@ -2,6 +2,6 @@ const primeiroNome = "Mario";
 const sobrenome = "";
 const apelido = "";
 
-if (apelido.length) console.log(apelido);
-else if (sobrenome.length) console.log(primeiroNome+" "+sobrenome); 
+if (apelido) console.log(apelido);
+else if (sobrenome) console.log(primeiroNome+" "+sobrenome); 
 else console.log(primeiroNome);
