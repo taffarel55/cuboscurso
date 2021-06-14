327461763771d99743459eb30f5759b69eab31d4
Mauricio Taffarel <mauruciotaffarel@gmail.com>
2021-06-14 19:40:26 -0300

    Identificando caracteres

--- Aula 5/back-integral-condicionais-main/extra-10/index.js
+++ Aula 5/back-integral-condicionais-main/extra-10/index.js
@@ -1,1 +1,4 @@
-const caractere = "E";
+const caractere = 'a';
+const vogais = ['A','E','I','O','U'];
+const numeros = [1,2,3,4,5,6,7,8,9,0]; 
+if(typeof caractere == 'string') console.log(`${vogais.indexOf(caractere.toUpperCase())!=-1?"Vogal":"Consoante"} ${caractere==caractere.toUpperCase()?"maiúscula":"minúscula"}`); else if (numeros.indexOf(caractere)!=-1) console.log(`Número`);
