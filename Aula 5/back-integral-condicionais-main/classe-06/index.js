b3a019ddc8cc98bbd8234a3289bd1ae2bca6fc51
Mauricio Taffarel <mauruciotaffarel@gmail.com>
2021-06-14 18:40:33 -0300

    Definindo posições

--- Aula 5/back-integral-condicionais-main/classe-06/index.js
+++ Aula 5/back-integral-condicionais-main/classe-06/index.js
@@ -1,3 +1,7 @@
 const alturaEmCm = 185;
 
-//seu código aqui
+if (alturaEmCm<180) console.log("REPROVADO");
+else if (alturaEmCm < 186) console.log("LÍBERO");
+else if (alturaEmCm < 196) console.log("PONTEIRO");
+else if (alturaEmCm < 206) console.log("OPOSTO");
+else console.log("CENTRAL");
