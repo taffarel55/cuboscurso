96b0bcdf8cf94fcaa5672b8b5c9b55d8969b603e
Mauricio Taffarel <mauruciotaffarel@gmail.com>
2021-06-16 16:42:26 -0300

    Contando letras

--- Aula 7/back-integral-arrays-e-loops-main/classe-02/index.js
+++ Aula 7/back-integral-arrays-e-loops-main/classe-02/index.js
@@ -1,1 +1,9 @@
 const letras = ["A", "a", "B", "C", "E", "e"];
+let num = 0;
+
+for(let i=0; i<letras.length; i++) {
+    if (letras[i].toUpperCase() == "E") num++;
+}
+
+if(num) console.log(`Foram encontradas ${num} 'E' ou 'e'`); 
+else console.log(`Não foram encontradas as letras 'E' ou 'e'`)
