2f990df92a681be20d8d2d490bc3946f4ccd768f
Mauricio Taffarel <mauruciotaffarel@gmail.com>
2021-06-16 17:17:18 -0300

    Fila do banco

--- Aula 7/back-integral-arrays-e-loops-main/extra-12/index.js
+++ Aula 7/back-integral-arrays-e-loops-main/extra-12/index.js
@@ -1,3 +1,11 @@
 const filaDeDentro = ["Jose", "Maria", "Joao"];
 const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
+let i=0;
 
+while(filaDeDentro.length<5) {
+    filaDeDentro.push(filaDeFora[0]);
+    filaDeFora.shift();
+    i++; 
+}
+
+console.log(filaDeDentro, filaDeFora);
