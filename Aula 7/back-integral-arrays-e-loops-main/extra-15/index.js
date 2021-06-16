f1ee91bd089190660b4b8f93840ab7735961651c
Mauricio Taffarel <mauruciotaffarel@gmail.com>
2021-06-16 18:08:03 -0300

    Separando pares de ímpares

--- Aula 7/back-integral-arrays-e-loops-main/extra-15/index.js
+++ Aula 7/back-integral-arrays-e-loops-main/extra-15/index.js
@@ -2,3 +2,9 @@ const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const pares = [];
 const impares = [];
 
+for (x of original) {
+    if (x%2) impares.push(x);
+    else pares.push(x);
+}
+
+console.log(pares,impares);
