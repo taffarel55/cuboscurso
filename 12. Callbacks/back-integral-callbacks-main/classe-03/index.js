6559547e0cf80661bd3e48339d3bdda8e9aa32a2
taffarel55 <mauruciotaffarel@gmail.com>
2021-07-01 11:17:29 -0300

    3

--- 12. Callbacks/back-integral-callbacks-main/classe-03/index.js
+++ 12. Callbacks/back-integral-callbacks-main/classe-03/index.js
@@ -1,1 +1,5 @@
 const frutas = ["Manga", "UVA", "abacaxi", "banaNA", "MAçã"];
+
+const frutasNova = frutas.map((x,i) => `${i} - ${x[0].toUpperCase()}${x.slice(1).toLowerCase()}`);
+
+console.log(frutasNova);
