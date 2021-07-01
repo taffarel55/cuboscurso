1540c9568db00b6f4f85256c86e0a1873ecd02aa
taffarel55 <mauruciotaffarel@gmail.com>
2021-07-01 15:42:55 -0300

    10

--- 12. Callbacks/back-integral-callbacks-main/extra-10/index.js
+++ 12. Callbacks/back-integral-callbacks-main/extra-10/index.js
@@ -1,1 +1,9 @@
-const notas = [6, 8, 9, 8];
+const notas = [6, 20, 33, 454, 8, 9, 9, 6, 8, 9, 20, 20, 33]
+
+notasUnicas = [];
+
+notas.forEach((x, i) => {
+	if (notas.indexOf(x)===i) notasUnicas.push(x);
+});
+
+console.log(notasUnicas);
