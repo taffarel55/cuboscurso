c7f8ffb039be3101357f693f9816b0c61885bc84
taffarel55 <mauruciotaffarel@gmail.com>
2021-07-01 10:13:29 -0300

    1

--- 12. Callbacks/back-integral-callbacks-main/classe-01/index.js
+++ 12. Callbacks/back-integral-callbacks-main/classe-01/index.js
@@ -1,1 +1,8 @@
 const cidades = ['Salvador', 'São Paulo', 'Brasilia', 'Recife', 'Rio de Janeiro'];
+
+const cidadesNova = cidades.filter(x=>{
+    if(x.length<9) return x;
+});
+
+console.log(cidadesNova);
+console.log(cidadesNova.join(', '));
