daaf30a7d1a2a66b60ef237e569e7721aa427bcd
taffarel55 <mauruciotaffarel@gmail.com>
2021-07-01 12:00:13 -0300

    6

--- 12. Callbacks/back-integral-callbacks-main/classe-06/index.js
+++ 12. Callbacks/back-integral-callbacks-main/classe-06/index.js
@@ -1,1 +1,10 @@
 const jogadores = ['Guido', 'Dina', 'Léo', 'Nanda', 'Juninho'];
+
+let i = jogadores.length;
+const ID = setInterval( () => {
+    if (i) console.log(jogadores[--i]);
+    else {
+        console.log('A rodada terminou!');
+        clearInterval(ID);
+    }
+},1000*(10/jogadores.length));
