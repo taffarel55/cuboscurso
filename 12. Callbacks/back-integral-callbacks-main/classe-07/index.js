292afc53d6e829dcbec56616bfa59326f9ccf6bd
taffarel55 <mauruciotaffarel@gmail.com>
2021-07-01 12:03:36 -0300

    7

--- 12. Callbacks/back-integral-callbacks-main/classe-07/index.js
+++ 12. Callbacks/back-integral-callbacks-main/classe-07/index.js
@@ -1,1 +1,10 @@
 const jogadores = ['Guido', 'Dina', 'Léo', 'Nanda', 'Juninho'];
+
+const fs = require('fs');
+
+fs.writeFile("meuarquivo.txt", "Estou aprendendo JavaScript na Cubos Academy", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("O arquivo foi salvo!");
+});
