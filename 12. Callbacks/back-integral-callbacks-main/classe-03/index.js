14fcc14313fb58e2df88fa25a419f501b9344365
taffarel55 <mauruciotaffarel@gmail.com>
2021-07-01 21:04:24 -0300

    3

--- 12. Callbacks/back-integral-callbacks-main/classe-03/index.js
+++ 12. Callbacks/back-integral-callbacks-main/classe-03/index.js
@@ -1,5 +1,3 @@
 const frutas = ["Manga", "UVA", "abacaxi", "banaNA", "MAçã"];
 
-const frutasNova = frutas.map((x,i) => `${i} - ${x[0].toUpperCase()}${x.slice(1).toLowerCase()}`);
-
-console.log(frutasNova);
+console.log(frutas.map((x,i) => `${i} - ${x[0].toUpperCase()}${x.slice(1).toLowerCase()}`));
--- 13. Sort e outros métodos/back-integral-sort-e-outros-metodos-main/classe-03/index.js
+++ 13. Sort e outros métodos/back-integral-sort-e-outros-metodos-main/classe-03/index.js
@@ -1,1 +1,3 @@
 const numeros = [1, 98, -76, 0, 12, 19, 5, 60, 44];
+
+console.log(numeros.reduce((acc,x)=>x>acc?x:acc));
