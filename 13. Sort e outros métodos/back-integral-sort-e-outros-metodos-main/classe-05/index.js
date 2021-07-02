e47d9d5a9d6822a43c8e61cfcc73829281875c04
taffarel55 <mauruciotaffarel@gmail.com>
2021-07-01 21:20:40 -0300

    5

--- 13. Sort e outros métodos/back-integral-sort-e-outros-metodos-main/classe-05/index.js
+++ 13. Sort e outros métodos/back-integral-sort-e-outros-metodos-main/classe-05/index.js
@@ -1,1 +1,3 @@
 const palavras = ["arroz", "feijão", "carne", "cerveja", "macarrão"];
+
+console.log(palavras.some((x)=>x==='cerveja'||x==='vodka')?'revise sua lista, joão. possui bebida com venda proibida!':'tudo certo, vamos as compras!');
