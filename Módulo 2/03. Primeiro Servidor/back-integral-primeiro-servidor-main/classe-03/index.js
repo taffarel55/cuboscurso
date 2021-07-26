e43048beb8de69bbb3de91f1a7a315c4484963c4
taffarel55 <mauruciotaffarel@gmail.com>
2021-07-26 20:53:28 -0300

    3

--- Módulo 2/03. Primeiro Servidor/back-integral-primeiro-servidor-main/classe-03/index.js
+++ Módulo 2/03. Primeiro Servidor/back-integral-primeiro-servidor-main/classe-03/index.js
@@ -1,5 +1,16 @@
-
-
-
+const express = require('express');
+const app = express();
 
 const jogadores = ["José", "Maria", "João", "Marcos", "Fernanda"];
+
+let i = 0;
+
+app.get('/', (req, res)=> {
+    let nomeDoJogador = jogadores[i];
+    res.send(`É a vez de ${nomeDoJogador} jogar!`);
+    if(++i===jogadores.length) i=0;
+});
+
+app.listen(8000,()=>{
+ console.log("Iniciando...");
+});
