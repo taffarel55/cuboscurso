19913544eb5c84e73f8436a4387110083732d9b3
taffarel55 <mauruciotaffarel@gmail.com>
2021-07-30 12:59:28 -0300

    Cadastro de alunos

--- Módulo 2/06. Rotas e Controladores/back-integral-rotas-e-controladores-main/classe-02/controladores/alunos.js
+++ Módulo 2/06. Rotas e Controladores/back-integral-rotas-e-controladores-main/classe-02/controladores/alunos.js
@@ -1,4 +1,5 @@
-const alunos = require('../dados/alunos')
+const alunos = require('../dados/alunos');
+const cursos = require('../dados/cursos');
 
 function getAlunos(req, res) {
     res.send(alunos);
@@ -56,14 +57,60 @@ function deleteAluno(req,res) {
     }
     const indice = alunos.indexOf(aluno);
     alunos.splice(indice,1);
+    res.status(202);
     res.json(aluno);
 }
 
-module.exports = {
-    getAlunos,
-    getAluno,
-    postAluno,
-    deleteAluno
+function editarAluno(req, res) {
+    const aluno = alunos.find(x=>x.id==req.params.id);
+    if(!aluno) {
+        res.status(404);
+        res.json({erro:"Aluno "+req.params.id+" não existe."});
+        return;
+    }
+    const mensagem = validarReq({
+        nome: req.body.nome ?? aluno.nome,
+        sobrenome: req.body.sobrenome ?? aluno.sobrenome,
+        idade: req.body.idade ?? aluno.idade,
+        curso: req.body.curso ?? aluno.curso,
+    });
+    if(mensagem) {
+        res.status(400);
+        res.json({ mensagem });
+        return;
+    }
+    if(req.body.nome !== undefined) aluno.nome = req.body.nome; 
+    if(req.body.sobrenome !== undefined) aluno.nome = req.body.sobrenomenome; 
+    if(req.body.idade !== undefined) aluno.idade = req.body.idade; 
+    if(req.body.curso !== undefined) aluno.curso = req.body.curso; 
+    res.json(aluno);
+}
+
+function substituirAluno(req, res) {
+    const mensagem = validarReq(req.body);
+    if(mensagem) {
+        res.status(400);
+        res.json({ mensagem });
+        return;
+    }
+    if(req.body.id!==Number(req.params.id)) {
+        res.status(400);
+        res.json({ erro: "O campo 'id' deve ser igual na rota e no corpo da requisição" });
+        return;
+    }
+    const aluno = alunos.find(x=>x.id==req.params.id);
+    if (aluno) {
+        if(req.body.nome !== undefined) aluno.nome = req.body.nome; 
+        if(req.body.sobrenome !== undefined) aluno.nome = req.body.sobrenomenome; 
+        if(req.body.idade !== undefined) aluno.idade = req.body.idade; 
+        if(req.body.curso !== undefined) aluno.curso = req.body.curso; 
+        res.json(aluno);
+    } else {
+        const novo = req.body;
+        alunos.push(novo);
+        res.status(201);
+        res.json(novo);
+    }
 }
 
 function validarReq(aluno) {
@@ -72,4 +119,15 @@ function validarReq(aluno) {
     if (!aluno.idade)            return "Idade não especificada";
     if (!aluno.curso.trim())     return "Curso não especificado";
     if (aluno.idade<18)          return "Aluno não pode ser menor de idade";
+
+    if (cursos.indexOf(aluno.curso)===-1) return "O curso não é válido";
+}
+
+module.exports = {
+    getAlunos,
+    getAluno,
+    postAluno,
+    deleteAluno,
+    editarAluno,
+    substituirAluno
 }
--- Módulo 2/06. Rotas e Controladores/back-integral-rotas-e-controladores-main/classe-02/roteador.js
+++ Módulo 2/06. Rotas e Controladores/back-integral-rotas-e-controladores-main/classe-02/roteador.js
@@ -1,5 +1,12 @@
 const express = require('express');
-const {getAlunos,getAluno,postAluno,deleteAluno} = require('./controladores/alunos')
+const {
+    getAlunos,
+    getAluno,
+    postAluno,
+    deleteAluno,
+    editarAluno,
+    substituirAluno
+} = require('./controladores/alunos')
 
 const roteador = express();
 
@@ -7,5 +14,7 @@ roteador.get('/alunos',getAlunos);
 roteador.get('/alunos/:id',getAluno);
 roteador.post('/alunos',postAluno);
 roteador.delete('/alunos/:id',deleteAluno);
+roteador.patch('/alunos/:id',editarAluno);
+roteador.put('/alunos/:id',substituirAluno);
 
 module.exports = roteador;
